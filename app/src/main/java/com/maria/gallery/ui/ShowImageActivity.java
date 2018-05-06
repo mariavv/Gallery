bc93273deb11e1878d4fb7aa625a9e18125b464b
vasilevamariav <vasilevamariav@yandex.ru>
2018-05-06 16:35:25 +0400

    fefore remove moxy

--- app/src/main/java/com/maria/gallery/ui/ShowImageActivity.java
+++ app/src/main/java/com/maria/gallery/ui/ShowImageActivity.java
@@ -4,12 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.maria.gallery.R;
@@ -20,11 +29,20 @@ public class ShowImageActivity extends MvpAppCompatActivity implements ShowImage
 
     public static final String ARG_FILE_DOWNLOAD_LINK = "fileDownloadLink";
 
+    private static final String KEY_TURN = "turn";
+
+    private static final Boolean DEFOULT = false;
+
     @InjectPresenter
     ShowImagePresenter presenter;
 
     SubsamplingScaleImageView image;
     ProgressBar progressBar;
+
+    /*
+    признак пересоздания активити
+    */
+    private Boolean turn;
 
     public static Intent createStartIntent(Context context, String fileDownloadLink) {
         Intent intent = new Intent(context, ShowImageActivity.class);
@@ -38,6 +56,8 @@ public class ShowImageActivity extends MvpAppCompatActivity implements ShowImage
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_image);
+
+        turn = savedInstanceState != null && savedInstanceState.getBoolean(KEY_TURN);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -80,6 +100,17 @@ public class ShowImageActivity extends MvpAppCompatActivity implements ShowImage
 
     private void setImage(ImageSource source) {
         stopProgressBar();
-        image.setImage(source);
+        if (!turn) {
+            image.setImage(source);
+        } else {
+            turn = DEFOULT;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean(KEY_TURN, true);
     }
 }
